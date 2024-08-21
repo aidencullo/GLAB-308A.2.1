2cf75c0cce2e3858ef6dfa9c5e290d0917e3400e
Aiden Cullo <aidencullo@gmail.com>
2024-08-21 15:37:04 -0400

    declare adventurer class

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-// Objectives
+g// Objectives
 // Use nested arrays and objects.
 // Combine objects, arrays, and functions.
 // Create a class to define the blueprint for creating objects.
@@ -112,3 +112,19 @@ robin.companion.type = "Cat";
 robin.companion.companion = new Character("Frank");
 robin.companion.companion.type = "Flea";
 robin.companion.companion.inventory = ["small hat", "sunglasses"];
+
+
+class Adventurer extends Character {
+  constructor (name, role) {
+    super(name);
+    // Adventurers have specialized roles.
+    this.role = role;
+    // Every adventurer starts with a bed and 50 gold coins.
+    this.inventory.push("bedroll", "50 gold coins");
+  }
+  // Adventurers have the ability to scout ahead of them.
+  scout () {
+    console.log(`${this.name} is scouting ahead...`);
+    super.roll();
+  }
+}
