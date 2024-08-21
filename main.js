c2975409d9f498baa375bc678aaab54fc7fd67bb
Aiden Cullo <aidencullo@gmail.com>
2024-08-21 15:28:11 -0400

    add roll method to robin object

--- main.js
+++ main.js
@@ -39,6 +39,26 @@
 // The companion has its own belongings, which includes a small hat and sunglasses.
 
 
+// const adventurer = {
+//   name: "Robin",
+//   health: 10,
+//   inventory: ["sword", "potion", "artifact"],
+//   companion: {
+//     name: "Leo",
+//     type: "Cat",
+//     companion: {
+//       name: "Frank",
+//       type: "Flea",
+//       inventory: ["hat", "sunglasses"],
+//     }
+//   }
+// }
+
+
+//redfine againe robin w functions
+
+
+
 const adventurer = {
   name: "Robin",
   health: 10,
@@ -51,5 +71,11 @@ const adventurer = {
       type: "Flea",
       inventory: ["hat", "sunglasses"],
     }
-  }
+  },
+  roll (mod = 0) {
+    const result = Math.floor(Math.random() * 20) + 1 + mod;
+    console.log(`${this.name} rolled a ${result}.`)
+  },
 }
+
+adventurer.roll(2)
