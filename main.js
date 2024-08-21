70ae25d4c5d3f756681d8d6bdaafa30fe2f46457
Aiden Cullo <aidencullo@gmail.com>
2024-08-21 15:35:56 -0400

    part 2 create character class

--- main.js
+++ main.js
@@ -59,28 +59,56 @@
 
 
 
-const adventurer = {
-  name: "Robin",
-  health: 10,
-  inventory: ["sword", "potion", "artifact"],
-  companion: {
-    name: "Leo",
-    type: "Cat",
-    companion: {
-      name: "Frank",
-      type: "Flea",
-      inventory: ["hat", "sunglasses"],
-    }
-  },
-  roll (mod = 0) {
-    const result = Math.floor(Math.random() * 20) + 1 + mod;
-    console.log(`${this.name} rolled a ${result}.`)
-  },
+// const adventurer = {
+//   name: "Robin",
+//   health: 10,
+//   inventory: ["sword", "potion", "artifact"],
+//   companion: {
+//     name: "Leo",
+//     type: "Cat",
+//     companion: {
+//       name: "Frank",
+//       type: "Flea",
+//       inventory: ["hat", "sunglasses"],
+//     }
+//   },
+//   roll (mod = 0) {
+//     const result = Math.floor(Math.random() * 20) + 1 + mod;
+//     console.log(`${this.name} rolled a ${result}.`)
+//   },
+// }
+
+// adventurer.roll();
+// adventurer.roll();
+// adventurer.roll();
+// adventurer.roll();
+// adventurer.roll();
+// adventurer.roll();
+
+
+
+
+// part 2 -- classes
+
+
+// Next, we will level up our approach using Classes.
+
+
+class Character {
+  constructor (name) {
+    this.name = name;
+    this.health = 100;
+    this.inventory = [];
+  }
 }
 
-adventurer.roll();
-adventurer.roll();
-adventurer.roll();
-adventurer.roll();
-adventurer.roll();
-adventurer.roll();
+
+// Now, we can re-create Robin using the Character class!
+
+const robin = new Character("Robin");
+robin.inventory = ["sword", "potion", "artifact"];
+robin.companion = new Character("Leo");
+robin.companion.type = "Cat";
+robin.companion.companion = new Character("Frank");
+robin.companion.companion.type = "Flea";
+robin.companion.companion.inventory = ["small hat", "sunglasses"];
