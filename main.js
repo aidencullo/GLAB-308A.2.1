6fd55adb761682716f7a4fe5863d2ce108036e5d
Aiden Cullo <aidencullo@gmail.com>
2024-08-21 15:12:30 -0400

    list objectives

---
+++ main.js
@@ -0,0 +1,11 @@
+// Objectives
+// Use nested arrays and objects.
+// Combine objects, arrays, and functions.
+// Create a class to define the blueprint for creating objects.
+// Add methods to a class.
+// Set properties on an instance of a class.
+// Make an instance of each class customizable.
+// Create methods to alter the properties of an instance.
+// Develop a class that inherits attributes from a "parent" class.
+// Create static properties for a class.
+// Create a "factory."
